3cc3601c4e8911ff5bf9839d94ba09f0faf88d87
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-11-21 14:53:01 +0100

    1

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,6 @@
-/*
-import type { LayoutServerLoad } from "./$types";
+import type { LayoutServerLoad } from './$types'
 
-import type { Actions } from "./$types";
-
-export const load = (async ({ url, locals: { safeGetSession } }) => {
+export const load = (async ({ url, locals: { safeGetSession }, cookies  }) => {
 	const { session, user } = await safeGetSession();
 
 	if (!user && url.pathname === "/kosik") {
@@ -12,24 +9,7 @@ export const load = (async ({ url, locals: { safeGetSession } }) => {
 
 	return {
 		session,
-		user
-	};
-}) satisfies LayoutServerLoad;
-*/
-
-import type { LayoutServerLoad } from "./$types";
-import { redirect } from "@sveltejs/kit";
-import type { Actions } from "./$types";
-
-export const load = (async ({ url, locals: { safeGetSession } }) => {
-	const { session, user } = await safeGetSession();
-
-	if (!user && url.pathname === "/kosik") {
-		throw redirect(302, "/");
-	}
-
-	return {
-		session,
-		user
+		user,
+		cookies: cookies.getAll(),
 	};
 }) satisfies LayoutServerLoad;
